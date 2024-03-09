906d24d511306fdcb89dd346b8c113c013b9b44d
Sanjay S Gangwar <contact.gangwar@gmail.com>
2024-03-09 17:34:09 +0530

    added binray search

--- src/main/kotlin/Algorithms/Others/BinarySearch.kt
+++ src/main/kotlin/Algorithms/Others/BinarySearch.kt
@@ -1,29 +1,30 @@
 package Algorithms.Others
 
-fun binarySearch(arr: IntArray, target: Int): Int {
+fun binarySearch(arr: IntArray, target: Int): Int? {
     var left = 0
     var right = arr.size - 1
-
     while (left <= right) {
         val mid = left + (right - left) / 2
-
-        if (arr[mid] == target)
+        if (arr[mid] == target) {
             return mid
-
-        if (arr[mid] < target)
+        }
+        if (arr[mid] < target) {
             left = mid + 1
-        else
+        } else {
             right = mid - 1
+        }
     }
-    return -1
+    return null
 }
 
 fun main() {
     val arr = intArrayOf(2, 3, 4, 10, 40)
-    val target = 10
+    val target = 12
     val result = binarySearch(arr, target)
-    if (result == -1)
+    result?.let {
+        println("Element found at index $it")
+    } ?: run {
         println("Element not present in array")
-    else
-        println("Element found at index $result")
+    }
+
 }
